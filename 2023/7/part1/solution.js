92bc48deafffea8d8414f7330e253cb1702c13c9
Viet Ngomai <thienvietngomai@gmail.com>
2023-12-20 12:30:12 -0700

    work on day 7 part 1

--- 2023/7/part1/solution.js
+++ 2023/7/part1/solution.js
@@ -7,6 +7,7 @@ const testCases = [
 	"KTJJT 220",
 	"QQQJA 483"
 ]
+var totalCases 
 
 // main(inputToList())
 test(testCases)
@@ -16,8 +17,19 @@ function test(input) {
 }
 
 function main(input) {
-	determineStrength(input)
+	// determineStrength(input)
+	totalCases = input.length
+	calculateWinnings()
 }
+
+var fullhands = []
+var fourhands = []
+var fullhousehands = []
+var threehands = []
+var twopairhands = []
+var onepairhands = []
+var highcardhands = []
+var allhands = [fullhands, fourhands,fullhousehands,threehands,twopairhands,onepairhands,highcardhands]
 
 function determineStrength(input) {
 	input.forEach(line => {
@@ -26,5 +38,34 @@ function determineStrength(input) {
 		var bid = result[1] 
 		console.log(hand)
 		console.log(bid)
+		for(var i=0; i<hand.length; i++) {
+			var card1 = hand[0]
+			var fullHand = Array(5).fill(card1).join("")
+			if(hand === fullHand) {
+				fullhands.push(result)
+			}
+		}
 	})
+}
+
+function compareHands(hand1, hand2) {
+	for(var i=0; i<hand1.length; i++) {
+		
+	}
+}
+
+function calculateWinnings(){
+	// var allhands = Array(1).fill(testCases) //line for testing purposes
+	var totalwinnings = 0;
+	var rank = totalCases;
+	console.log(rank)
+	allhands.forEach(handtype => {
+		for(var i=0; i<handtype.length; i++) {
+			var bid = handtype[i].split(" ")[1]		
+			totalwinnings+= (rank*bid)
+			rank--;
+			console.log(totalwinnings)
+		}
+	})
+	console.log("Total winnings: "+totalwinnings)
 }
